3f86cf0fe2c9ec15daee98582c688421157da0ab
dunz <zergcake@gmail.com>
2023-06-11 14:41:16 +0900

    ui challenge 2 - devtool, Container, BoxDecoration

--- lib/main.dart
+++ lib/main.dart
@@ -9,41 +9,84 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xFF1F2123),
-          body: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 80,
-                ),
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
+        backgroundColor: const Color(0xFF1F2123),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 80,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
                     children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          const Text(
-                            'Hi DJ',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text('Welcome back',
-                            style: TextStyle(
-                              color: Colors.white.withOpacity(.8),
-                            ),
-                          ),
-                        ],
-                      )
-                    ]
-                )
-              ],
-            )
-          )
-      )
+                      const Text(
+                        'Hi DJ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'Welcome back',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(.8), fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Text('Total Balance',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white.withOpacity(0.5),
+                  )),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.amber,
+                      borderRadius: BorderRadius.circular(40),
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 24,
+                        vertical: 16,
+                      ),
+                      child: Text(
+                        'Transfer',
+                        style: TextStyle(fontSize: 22),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
